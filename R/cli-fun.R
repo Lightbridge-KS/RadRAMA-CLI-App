a08fff631f35ecc41bca393d88f2b67beb28a87a
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-06-24 19:22:45 +0700

    polish cli functions

--- R/cli-fun.R
+++ R/cli-fun.R
@@ -1,4 +1,14 @@
 
+#' Cat Text
+#'
+#' Cat text for specified repitition and total time
+#'
+#' @param text text to cat
+#' @param rep how many cycle
+#' @param total_time total time (sec)
+#'
+#' @return NULL
+#' @export
 cat_progress_rep <- function(text = ".\n", rep = 2, total_time = 1) {
     cycle <- total_time / rep
 
@@ -9,15 +19,28 @@ cat_progress_rep <- function(text = ".\n", rep = 2, total_time = 1) {
 }
 
 
-spin_custom <- function(sp,
-                        total_time = 1,
-                        frame = 100
+#' Spin Custom
+#'
+#' Spine a spinner for specified duration and frame rate
+#'
+#' @param (cli_spinner) A spinner object
+#' @param total_sec total time to spin (seconds)
+#' @param frame_rate frame rate (frames/seconds)
+#'
+#' @return
+#' @export
+#'
+spin_custom <- function(sp = cli::make_spinner(),
+                        total_sec = 1,
+                        frame_rate = 100
 ) {
 
-  cycle <- total_time / frame
-  lapply(1:frame, function(x) {
+  frames <- frame_rate * total_sec
+  cycle_sec <- 1 / frame_rate   # Second per cycle
+
+  lapply(1:frames, function(x) {
     sp$spin()
-    Sys.sleep(cycle)
+    Sys.sleep(cycle_sec)
   })
   sp$finish()
 }
